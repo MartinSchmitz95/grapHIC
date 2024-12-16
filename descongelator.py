9b465218181800a9e6afae9e3d2f445e3c634022
Leon Rauschning <99650940+lrauschning@users.noreply.github.com>
2024-12-16 15:36:09 +0800

    commit initial descongelator implementation

--- descongelator.py
+++ descongelator.py
@@ -1,8 +1,15 @@
 #!/bin/python
 
 import sys
+import tempfile
+import math
+
+import numpy as np
+
+from typing import Tuple
 
 import cooler
+from cooler import Cooler
 
 ## THOUGHTS
 # file formats:
@@ -15,5 +22,52 @@ import cooler
 # https://github.com/open2c/cooler/blob/cfd2c4990a2c52deb5f3d277c977daae6e59f6c4/src/cooler/cli/coarsen.py#L58
 # then export as numpy matrix
 
-infile = sys.argv[1]
-#outfile = sys.argv[2]
+def aggr_chrs(in_path: str) -> Cooler:
+    """
+    Takes a path to a cooler in any resolution, calls coarsen to aggregate counts by chromosome.
+    :returns: A new Cooler stored in a tmpfile with counts aggregated by chromosome.
+    """
+    # cooler requires a URI on the filesystem, so use a tmpfile
+    tmp = tempfile.NamedTemporaryFile().name
+
+    cooler.coarsen_cooler(in_path, tmp, sys.maxsize, 900) # factor should be infinity
+
+    return Cooler(tmp)
+
+
+def to_np_matrix(in_cooler: Cooler) -> Tuple[np.array, np.ndarray]:
+    """
+    Extracts the data in a cooler aggregated by chromosomes into a numpy matrix.
+    :returns: a tuple of a 1D numpy array containing chromosome labels and a 2D array containing counts between chromosomes.
+    """
+    # this returns it in a bit of a weird format, necessitating [:]
+    # could also call fetch(chr, 0, 1) in a loop, but I think this is neater
+    array = np.array(in_cooler.matrix(balance=False)[:])
+    chrnames = np.array(in_cooler.chromnames)
+
+    if len(array) != len(chrnames):
+        raise ValueError("Labels not same length as array! Might not have been aggregated by Chr.")
+
+    return (chrnames, array)
+
+def save_np_matrix(intuple, path, sep='\t'):
+    np.savetxt(path, intuple[1], fmt='%d', delimiter=sep, header='#' + sep.join(intuple[0]))
+
+
+def main(args):
+    #TODO do proper argparsing later
+    infile = args[1]
+    outfile = args[2]
+
+    np_tup = to_np_matrix(
+            aggr_chrs(infile)
+            )
+
+    print(np_tup)
+
+    save_np_matrix(np_tup, outfile)
+
+
+
+if __name__ == '__main__':
+    main(sys.argv)
