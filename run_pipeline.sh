fe212ccc042517459df16949f1ed8ebb7814da96
Leon Rauschning <99650940+lrauschning@users.noreply.github.com>
2025-04-02 16:51:00 +0800

    add run_pipeline.sh

---
+++ run_pipeline.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## make sure we are in an env with appropriate tools installed
+## apart from nextflow, needed are (should all be on bioconda)
+## - bowtie2
+## - hifiasm
+## - fastqc
+## - multiqc
+## - python
+## - cooler
+micromamba activate nf-core
+
+nextflow ~/graphic/main.nf --input $1 -bg --dnase --outdir $(basename $1 .csv)
+# -profile conda # if not running in a conda env above
